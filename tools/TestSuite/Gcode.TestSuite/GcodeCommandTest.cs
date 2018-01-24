4547ba6b9051f1910f67bd85f2e9cf1dd8322aff
Anton Maisak <am@rus-bit.com>
2018-01-24 21:00:15 +0300

    added Gcode.TestSuite

---
+++ tools/TestSuite/Gcode.TestSuite/GcodeCommandTest.cs
@@ -0,0 +1,27 @@
+﻿using Gcode.Entity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gcode.TestSuite {
+	/// <summary>
+	/// Gcode command tests
+	/// </summary>
+	[TestClass]
+	public class GcodeCommandTest {
+		/// <summary>
+		/// Gcode command
+		/// </summary>
+		private GcodeCommand _gcodeCommand;
+		[TestMethod]
+		public void CheckInstanceType() {
+			_gcodeCommand = new GcodeCommand();
+			
+			Assert.IsInstanceOfType(_gcodeCommand, typeof(GcodeCommand));
+		}
+		[TestMethod]
+		public void CheckInstanceNameTypeStr() {
+			_gcodeCommand = new GcodeCommand();
+			var t = _gcodeCommand.ToString();
+			Assert.AreEqual(t, "Gcode.Entity.GcodeCommand");
+		}
+	}
+}
---
+++ tools/TestSuite/Gcode.TestSuite/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gcode.TestSuite")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Gcode.TestSuite")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("50cc568e-8cd4-4082-a79a-daf2e14fc2ab")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
