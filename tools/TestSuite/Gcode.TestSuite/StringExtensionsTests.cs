a5fe9298d03dabe2789e1f70e8e024b7d288c88b
Anton Maisak <am@rus-bit.com>
2018-01-29 22:21:10 +0300

    TrimStringTest

---
+++ tools/TestSuite/Gcode.TestSuite/StringExtensionsTests.cs
@@ -0,0 +1,21 @@
+﻿using Gcode.Utils.Infrastructure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gcode.TestSuite {
+	[TestClass]
+	public class StringExtensionsTests {
+
+
+		[TestMethod]
+		public void TrimStringTest() {
+			var s = string.Empty;
+			for (var i = 0; i < 1000; i++) {
+				if (i > 0) {
+					s += " ";
+					Assert.IsTrue(s.TrimString() == string.Empty);
+				}
+			}
+
+		}
+	}
+}
