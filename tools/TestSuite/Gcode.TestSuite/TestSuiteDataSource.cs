3415af5e551638607d9ec7275c67445790dfd90b
Anton Maisak <am@rus-bit.com>
2018-01-31 11:11:35 +0300

    DataSource over https

--- tools/TestSuite/Gcode.TestSuite/TestSuiteDataSource.cs
+++ tools/TestSuite/Gcode.TestSuite/TestSuiteDataSource.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.Net;
 
 namespace Gcode.TestSuite
 {
@@ -6,13 +7,21 @@ namespace Gcode.TestSuite
 	{
 		public static string GetDataSource(string fileName)
 		{
-			var filePath = $"{Directory.GetCurrentDirectory()}\\..\\..\\..\\..\\..\\misc\\gcode\\{fileName}";
-			if (File.Exists(filePath))
-			{
-				var fileContent = File.ReadAllText(filePath);
-				return fileContent;
-			}
-			return string.Empty;
+			var uri = new Uri($"https://raw.githubusercontent.com/rus-bit/Gcode/master/misc/gcode/{fileName}");
+#pragma warning disable SecurityIntelliSenseCS // MS Security rules violation
+			var wc = new WebClient().DownloadString(uri);
+#pragma warning restore SecurityIntelliSenseCS // MS Security rules violation
+
+			return wc;
+
+			//var filePath = $"{Directory.GetCurrentDirectory()}\\..\\..\\..\\..\\..\\misc\\gcode\\{fileName}";
+
+			//if (File.Exists(filePath))
+			//{
+			//	var fileContent = File.ReadAllText(filePath);
+			//	return fileContent;
+			//}
+			//return string.Empty;
 		}
 
 	}
